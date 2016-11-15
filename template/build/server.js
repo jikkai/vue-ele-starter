039c397ac23617ccbbd116724a4c6485b8f6d834
Jikkai Xiao <sonne@asaki.me>
2016-11-15 18:27:47 +0800

    support hot reload

--- template/build/server.js
+++ template/build/server.js
@@ -8,6 +8,11 @@ const config = require('./config')
 
 const app = express()
 
+webpackConfig.entry.client = [
+  'webpack-hot-middleware/client',
+  webpackConfig.entry.client
+]
+
 const compiler = webpack(webpackConfig)
 
 const devMiddleWare = require('webpack-dev-middleware')(compiler, {
