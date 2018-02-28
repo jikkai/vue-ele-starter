34437a8dd9b7ecd9281d671430f3fca865fa3f7e
Jikkai Xiao <sonne@asaki.me>
2018-02-28 10:55:49 +0800

    modified babelrc

--- template/postcss.config.js
+++ template/postcss.config.js
@@ -1,12 +1,22 @@
 module.exports = {
   plugins: {
     'postcss-salad': {
+      browsers: [
+        "> 1%",
+        "last 2 versions",
+        "not ie <= 8"
+      ],
       features: {
         bem: {
           shortcuts: { component: 'c', modifier: 'm', descendent: 'd' },
           separators: { modifier: '--', descendent: '__' }
-        }
+        },
+        autoprefixer: false
       }
+    },
+    cssnano: {
+      reduceIdents: false,
+      zindex: false
     }
   }
 }
